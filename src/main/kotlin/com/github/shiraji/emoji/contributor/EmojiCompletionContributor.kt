c9c77b8abb8f5c9b9c5786ec85a7861e7d2c27b3
shiraji <isogai.shiraji@gmail.com>
2017-05-11 11:30:46 +0900

    Support retina

--- src/main/kotlin/com/github/shiraji/emoji/contributor/EmojiCompletionContributor.kt
+++ src/main/kotlin/com/github/shiraji/emoji/contributor/EmojiCompletionContributor.kt
@@ -3,28 +3,23 @@ package com.github.shiraji.emoji.contributor
 import com.github.shiraji.emoji.data.EmojiData
 import com.intellij.codeInsight.completion.*
 import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.openapi.util.IconLoader
 import com.intellij.patterns.PlatformPatterns
 import com.intellij.psi.PsiPlainText
 import com.intellij.util.ProcessingContext
-import com.intellij.util.ui.UIUtil
-import java.awt.Image
 import java.io.File
-import javax.imageio.ImageIO
-import javax.swing.ImageIcon
 
 class EmojiCompletionContributor : CompletionContributor() {
 
     val emojiList = mutableListOf<EmojiData>()
 
     init {
-        val url = javaClass.getResource("/emoji/public/graphics/emojis")
-        val emojiDir = File(url.toURI())
-        emojiDir.list().forEach {
-            val image = ImageIO.read(File(emojiDir.absolutePath + "/" + it))
-            val size = if (UIUtil.isRetina()) 32 else 16
-            val scaledImage = image.getScaledInstance(size, size, Image.SCALE_SMOOTH)
-            emojiList.add(EmojiData(":${it.replaceAfter(".", "").replace(".", "")}:", ImageIcon(scaledImage)))
-        }
+        File(javaClass.getResource("/icons").toURI()).list()
+                .filterNot { it.contains("@2") }
+                .forEach {
+                    val icon = IconLoader.getIcon("/icons/$it")
+                    emojiList.add(EmojiData(it.replaceAfter(".", "").replace(".", ""), icon))
+                }
 
         extend(CompletionType.BASIC, PlatformPatterns.psiElement(PsiPlainText::class.java), object : CompletionProvider<CompletionParameters>() {
             override fun addCompletions(parameters: CompletionParameters, context: ProcessingContext?, result: CompletionResultSet) {
